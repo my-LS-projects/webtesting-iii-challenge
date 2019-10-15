5a7f42965341be882f203dcb3e1fef70db35bf2a
fresco <fresco@frescos-iMac.local>
2019-10-15 00:52:56 -0400

    MVP I guess? dunno what else to test sooo

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import Display from '../display/Display'
 
@@ -7,3 +7,18 @@ test('display renders', () => {
     render(<Display />)
 })
 
+test('open/closed test', () => {
+    const lockedMock = jest.fn()
+    const closedMock = jest.fn()
+    const { getByText } = render(<Display closed={closedMock} locked={lockedMock} />)
+    lockedMock ? getByText(/locked/i) : getByText(/unlocked/i)
+    closedMock ? getByText(/closed/i) : getByText(/open/i)
+})
+
+test('led color', () => {
+    const lockedMock = jest.fn()
+    const closedMock = jest.fn()
+    const { getByText } = render(<Display locked={lockedMock} closed={closedMock}/>)
+    lockedMock ? expect(getByText(/locked/i).classList.contains('red-led')).toBeTruthy() : expect(getByText(/unlocked/i).classList.contains('green-led')).toBeTruthy()
+    closedMock ? expect(getByText(/closed/i).classList.contains('red-led')).toBeTruthy() : expect(getByText(/open/i).classList.contains('green-led')).toBeTruthy()
+})
